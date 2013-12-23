18885298806b9bcdbace6ee45f5c220e588f58be
Calvin French-Owen <calvin@calv.info>
2013-12-22 22:21:25 -0800

    voting properly

--- test/test.js
+++ test/test.js
@@ -3,17 +3,17 @@ var Node = require('cruise-node');
 
 
 var node1 = new Node()
-  .state(states.Follower)
+  .state('follower')
   .addPeer('127.0.0.1', 4006)
   .addPeer('127.0.0.1', 4007);
 
 var node2 = new Node()
-  .state(states.Follower)
+  .state('follower')
   .addPeer('127.0.0.1', 4005)
   .addPeer('127.0.0.1', 4007);
 
 var node3 = new Node()
-  .state(states.Follower)
+  .state('follower')
   .addPeer('127.0.0.1', 4005)
   .addPeer('127.0.0.1', 4006);
 
@@ -21,3 +21,20 @@ var node3 = new Node()
 node1.listen(4005);
 node2.listen(4006);
 node3.listen(4007);
+
+
+
+setInterval(function () {
+  node1.stop();
+  setTimeout(function () { node1.listen(4005) }, 800);
+}, 3500);
+
+setInterval(function () {
+  node2.stop();
+  setTimeout(function () { node2.listen(4006) }, 800);
+}, 4100);
+
+setInterval(function () {
+  node3.stop();
+  setTimeout(function () { node3.listen(4007) }, 800);
+}, 4500);
